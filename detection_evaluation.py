cff0dd1d7be269ccda9f9412019e032766d710d8
yangnk <yangningkai@qq.com>
2017-06-27 21:20:02 +0800

    完成对整个目录下求解每个文件的recall和precision，但是在打印average 的时候有问题

--- detection_evaluation.py
+++ detection_evaluation.py
@@ -69,87 +69,111 @@ def match_num(list1, list2):
     return num
 
 def init():
-    pass
-
-if __name__ == "__main__":
-    init()
-    path_l = os.path.join(os.getcwd(), "data/3_l.txt")
-    path_d = os.path.join(os.getcwd(), "data/3_d.txt")
-    path_result = os.path.join(os.getcwd(), "result/result.txt")
-    file_l = open(path_l);
-    file_d = open(path_d);
-    list_l = file_l.readlines();
-    list_d = file_d.readlines();
+    path = os.getcwd()
+    path_l = os.path.join(path, "data/label_file_test")
+    path_d = os.path.join(path, "data/detect_file_test")
+    listdir_l = os.listdir(path_l)
+    listdir_d = os.listdir(path_d)
+    listdir_abs_l = [path_l + "/" + x for x in listdir_l]
+    listdir_abs_d = [path_d + "/" + x for x in listdir_d]
 
     # 判定result文件是否存在，若否则新建
     if os.path.exists(os.path.join(os.getcwd(), "result/")) == False:
         os.mkdir(os.path.join(os.getcwd(), "result/"))
-    if os.path.exists(os.path.join(os.getcwd(), "result/result.txt")) == False:
-        os.system("touch result/result.txt")
-        os.system("chmod 770 result/result.txt")
-    file_result = open(path_result, "w+")
+    if os.path.exists(os.path.join(os.getcwd(), "result/result.txt")) == True:
+        os.remove(os.path.join(os.getcwd(), "result/result.txt"))
+    # os.mknod(os.path.join(os.getcwd(), "result/result.txt"))
+    os.system("touch result/result.txt")
+    os.system("chmod 770 result/result.txt")
+    # file_result = open(path_result, "w+")
+    return listdir_abs_l, listdir_abs_d
 
-    dict_l = {};
-    dict_d = {};
-    for item in list_l:
-        x = item.split();
-        char = x[1];
-        info_0 = x[2:];
-        info = map(int, info_0)
-        #dict_l[char] = info
-        # dict_l.setdefault(,)
-        dict_l.setdefault(char, []).append(info)
-    for item in list_d:
-        x = item.split();
-        char = x[1];
-        info_0 = x[2:];
-        info = map(int, info_0)
-        # dict_d[char] = info;
-        dict_d.setdefault(char, []).append(info)
+if __name__ == "__main__":
+    (listdir_abs_l, listdir_abs_d) = init()
+    name_result = os.path.join(os.getcwd(), "result/result.txt")
+    file_result = open(name_result,"w+")
+    cnt_valid = 0
+    sum_recall = 0
+    sum_precision = 0
 
-    dict_len_l = multi_dict_len(dict_l)
-    dict_len_d = multi_dict_len(dict_d)
-
-    # 计算recall
-    cnt_bingo = 0
-    for x in dict_l:
-        if dict_d.has_key(x):
-            cnt_bingo += 1
-        #    if dict_d.has_key(x) == True:
-        #        cnt += match_num(dict_l[x], dict_d[x])
-        #        # cnt += 1
-        # # else:
-        #     if dict_d.has_key(x) == False:
-        #         continue
-        #     else:
-        #         # multi_len = len(dict_l[x])
-        #         cnt += match_num(dict_l[x], dict_d[x])
-
-    # cnt_d = 0;
-    # cnt_l = dict_len_l
-    # for x in dict_l:
-    #     if x in dict_d:
-    #         cnt_d = cnt_d + 1;
-    recall = cnt_bingo / dict_len_l
-
-    # 计算precision
-    # 判定被正确标注的数量，通过判定label中字符是否在detect出现，分字符只出现一次和出现多次两类处理
-    cnt_pefect = 0
-    for x in dict_l:
-        # if len(dict_l[x]) == 1:
-        if dict_d.has_key(x) == False:
+    for path_abs_l in listdir_abs_l:
+        name_l = path_abs_l.split('/')[-1]
+        if name_l.split('.')[-1] != "txt":
+            continue
+        elif name_l in [x.split('/')[-1] for x in listdir_abs_d] == False:
+            file_result.writelines(name_l)
+            file_result.writelines("none")
             continue
         else:
-            cnt_pefect += match_num(dict_l[x], dict_d[x])
-    precision = cnt_pefect / cnt_bingo
+            file_l = open(path_abs_l)
+            path_abs_d = os.getcwd() + "/data/detect_file_test/" + name_l
+            file_d = open(path_abs_d);
+            list_l = file_l.readlines();
+            list_d = file_d.readlines();
 
-    result_recall = "recall=%.3f" % recall
-    result_precision = "precision=%.3f" % precision
-    print result_recall
-    print result_precision
-    file_result.writelines(result_recall)
-    file_result.writelines(result_precision)
+            dict_l = {};
+            dict_d = {};
+            for item in list_l:
+                one = item.split();
+                char = one[1];
+                info_0 = one[2:];
+                info = map(int, info_0)
+                # dict_l[char] = info
+                # dict_l.setdefault(,)
+                dict_l.setdefault(char, []).append(info)
+            for item in list_d:
+                one = item.split();
+                char = one[1];
+                info_0 = one[2:];
+                info = map(int, info_0)
+                # dict_d[char] = info;
+                dict_d.setdefault(char, []).append(info)
+
+            dict_len_l = multi_dict_len(dict_l)
+            dict_len_d = multi_dict_len(dict_d)
+
+            # 计算recall
+            cnt_bingo = 0
+            dict_new_l = {}
+            for dict_one in dict_l:
+                if len(dict_l[dict_one]) == 1:
+                    if dict_d.has_key(dict_one):
+                        cnt_bingo += 1
+                        dict_new_l[dict_one] = dict_l[dict_one]
+                else:
+                    if dict_d.has_key(dict_one):
+                        dict_new_l[dict_one] = dict_l[dict_one]
+                        cnt_bingo += len(dict_l[dict_one])
+            recall = cnt_bingo / dict_len_l
+
+            # 计算precision
+            # 判定被正确标注的数量，通过判定label中字符是否在detect出现，分字符只出现一次和出现多次两类处理
+            cnt_pefect = 0
+            for dict_one in dict_new_l:
+                # if len(dict_l[x]) == 1:
+                # if dict_d.has_key(dict_one) == False:
+                #     continue
+                # else:
+                cnt_pefect += match_num(dict_l[dict_one], dict_d[dict_one])
+            precision = cnt_pefect / cnt_bingo
+            cnt_valid += 1
+            sum_recall += recall
+            sum_precision += precision
+
+            result_recall = "recall=%.3f" % recall
+            result_precision = "precision=%.3f" % precision
+            result = "%s %s %s" % (result_recall, result_precision, name_l)
+            print result
+            file_result.writelines(result + "\n")
+            file_result.flush()
+    result_recall_average = "average recall=%.3f" % sum_recall / cnt_valid
+    result_precision_average = "average precision=%.3f" % sum_precision / cnt_valid
+    result_average = "============\n"
+    result_average += "%s %s" % (result_recall, result_precision, name_l)
+    print result_average
+    file_result.writelines(result_average + "\n")
     file_result.flush()
+
     file_l.close()
     file_d.close()
     file_result.close()
